e7f3f50781f3d03b7d75575d276ea6c559fe668f
Leosly <Leonardonigro11@gmail.com>
2024-03-03 18:53:47 -0500

    test6

--- api/index.py
+++ api/index.py
@@ -42,7 +42,7 @@ def fetch_json_data(file_path, commit_url):
 
 @app.route('/api/json/<commit_id>', methods=['GET'])
 def get_json_data(commit_id):
-        return jsonify(stored_data[commit_id])
+        return jsonify(stored_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
